ec56ab244c12a124457aabee2acd6edcc5874b2b
Chawanat Nakasan <chawanat.n@gmail.com>
2016-06-23 00:00:08 +0900

    test configs are now decoupled from mptoolcp itself

--- tests/smoctest/smoctest.sh
+++ tests/smoctest/smoctest.sh
@@ -4,7 +4,7 @@ test_dir="$(dirname "$0")"
 mptoolcp_dir="$(dirname "$0")/../.."
 parent_path=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
 
-source ${mptoolcp_dir}/mptoolcp.sh
 source ${test_dir}/config.sh
+source ${mptoolcp_dir}/mptoolcp.sh
 
 
